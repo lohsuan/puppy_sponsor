e64dd60ed65ba1ca2f93104d45e702d47195a5a7
Xanonymous <trusaidlin@gmail.com>
2022-06-07 23:59:17 +0800

    chore: enhance eslint config setups

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,9 +29,57 @@ module.exports = {
         varsIgnorePattern: '^h$'
       }
     ],
-    'space-before-function-paren': 'off',
     quotes: ['error', 'single'],
-    'comma-dangle': ['error', 'never']
+    'comma-dangle': ['error', 'never'],
+    semi: ['error', 'never'],
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true
+      }
+    ],
+    'no-param-reassign': ['error', { props: false }],
+    'array-bracket-newline': ['error', 'consistent'],
+    'array-element-newline': ['error', 'consistent'],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: {
+          minProperties: 3,
+          multiline: true,
+          consistent: true
+        },
+        ObjectPattern: {
+          minProperties: 3,
+          multiline: true,
+          consistent: true
+        },
+        ImportDeclaration: {
+          minProperties: 3,
+          multiline: true,
+          consistent: true
+        },
+        ExportDeclaration: {
+          minProperties: 3,
+          multiline: true,
+          consistent: true
+        }
+      }
+    ],
+    'object-property-newline': [
+      'error',
+      {
+        allowAllPropertiesOnSameLine: false
+      }
+    ],
+    'space-before-function-paren': ['error', 'always'],
+    // import
+    'import/order': 'error',
+    'import/first': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-default-export': 'off'
   },
   settings: {
     react: {
