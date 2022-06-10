3faf1bc2b52331486b3a0acf8adee0238a2657b5
Xanonymous <trusaidlin@gmail.com>
2022-06-10 09:35:54 +0800

    feat: add img upload feature

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,12 +1,62 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import Datepicker from 'flowbite-datepicker/Datepicker'
+import { isImageFile, uploadMedia } from '../utils/img'
 
+/**
+ * Content management pages for third-party agencies.
+ * This page can interact with all contract methods that require an owner EOA.
+ *
+ * @returns {JSXElement} The AdminPage Component.
+ * */
 const AdminPage = () => {
+  const [newPuppyName, setNewPuppyName] = useState('')
+  const [newPuppyBirthday, setNewPuppyBirthday] = useState('')
+  const [newPuppyDesc, setNewPuppyDesc] = useState('')
+  const [newPuppyImgUrl, setNewPuppyImgUrl] = useState('https://i.imgur.com/V8G7aBN.webp')
+  const [isProcessing, setIsProcessing] = useState(false)
+  const fileUploader = useRef(null)
+
   const setupDatePickers = useCallback(() => {
     // TODO: inspect why DatePicket's style could not be applied.
     const datepickerEl = document.getElementById('new-puppy-date-picker')
     new Datepicker(datepickerEl)
   }, [])
+
+  const uploadButtonClicked = () => {
+    if (isProcessing) {
+      return
+    }
+
+    fileUploader.current.click()
+  }
+
+  /**
+   * @param {InputEvent} e
+   * */
+  const onDesireUploadFileChosen = async (e) => {
+    if (isProcessing) {
+      return
+    }
+
+    const file = e.target.files[0]
+    if (!isImageFile(file)) {
+      return
+    }
+
+    setIsProcessing(true)
+    const { url } = await uploadMedia(file, 'webp')
+    setIsProcessing(false)
+
+    if (url) {
+      setNewPuppyImgUrl(url)
+    }
+  }
+
+  const onCreatePuppyClicked = () => {
+    if (isProcessing) {
+      return
+    }
+  }
 
   useEffect(() => {
     // setupDatePickers()
@@ -18,9 +68,17 @@ const AdminPage = () => {
         <p className="text-center text-white font-extrabold text-2xl my-5 md:my-2">New Puppy</p>
         <div className="flex flex-col items-center md:flex-row">
           <img
-            className="my-2 cursor-pointer opacity-20 p-5 border-dashed border-4 border-gray-400 hover:border-amber-300 max-h-40 md:max-h-max md:h-auto object-cover w-auto rounded-t-lg md:w-1/3 md:rounded-none md:rounded-l-lg"
-            src="https://i.imgur.com/yk9WRbT.webp"
-            alt=""
+            className="my-2 cursor-pointer p-5 border-dashed border-4 border-gray-400 hover:border-amber-300 max-h-40 md:max-h-max md:h-auto object-cover w-auto rounded-t-lg md:w-1/3 md:rounded-none md:rounded-l-lg"
+            src={newPuppyImgUrl}
+            onClick={uploadButtonClicked}
+            alt="newPuppyImg"
+          />
+          <input
+            onInput={onDesireUploadFileChosen}
+            ref={fileUploader}
+            className="hidden"
+            type="file"
+            accept="image/jpeg,image/png,image/gif"
           />
           <div className="w-full flex flex-col justify-between p-4 leading-normal">
             <div className="mb-6">
@@ -35,6 +93,7 @@ const AdminPage = () => {
                 id="new-puppy-name"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="My lovely dog :)"
+                onChange={(el) => setNewPuppyName(el.target.value)}
                 required
               />
             </div>
@@ -65,6 +124,7 @@ const AdminPage = () => {
                   type="text"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Select date"
+                  onChange={(el) => setNewPuppyBirthday(el.target.value)}
                 />
               </div>
             </div>
@@ -81,13 +141,16 @@ const AdminPage = () => {
                 rows="4"
                 className="resize-none block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="About this puppy..."
+                onChange={(el) => setNewPuppyDesc(el.target.value)}
               />
             </div>
 
             <div className="flex items-start mb-6">
               <button
                 type="button"
-                className="text-white bg-[#FF9119] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 mr-2 mb-2"
+                disabled={isProcessing || newPuppyName.trim() === ''}
+                onClick={onCreatePuppyClicked}
+                className="text-white disabled:bg-gray-400 disabled:opacity-20 bg-[#FF9119] enabled:hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center enabled:dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 mr-2 mb-2"
               >
                 <svg
                   className="w-4 h-4 mr-2 -ml-1 text-[#626890]"
