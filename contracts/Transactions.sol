8a3f6566a215ad77d1d3be4635076983effa32de
lohsuan <angelalo88362@gmail.com>
2022-06-10 18:27:47 +0800

    feat: switch to new constract.

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.14;
 
 // TODO: Comprehensive Security Check, refer to https://consensys.github.io/smart-contract-best-practices
 contract Transactions {
-    address public owner;
+    address payable public owner;
     uint256 public transactionCount;
     uint256 public totalAmount;
 
     event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword, string metaData);
-    event PuppyAdded(string dogId, string imageUrl, uint256 timestamp);
+    event PuppyAdded(bytes32 dogId, string imageUrl, string name, string birthday, string description, uint256 timestamp);
 
     struct TransferStruct {
         address sender;
@@ -18,19 +18,23 @@ contract Transactions {
         string message;
         uint256 timestamp;
         string keyword;
-        string metaData; // dogId, -1 for donate food
+        string dogId; // dogId, -1 for donate food
     }
 
     struct Puppy {
-        string dogId;
+        bytes32 dogId;
         string imageUrl;
+        string name;
+        string birthday;
+        string description;
     }
 
     TransferStruct[] transactions;
+    mapping(bytes32 => bool) puppyIdSet;
     Puppy[] puppies;
-    mapping(string => TransferStruct[]) puppyMap;  // dogId => Transaction[]
 
-    constructor(address _owner) {
+    constructor(address payable _owner) {
+        require(_owner == address(_owner));
         owner = _owner;
     }
 
@@ -39,7 +43,8 @@ contract Transactions {
         _;
     }
 
-    function setOwner(address _newOwner) public onlyOwner {
+    function setOwner(address payable _newOwner) public onlyOwner {
+        require(_newOwner == address(_newOwner));
         owner = _newOwner;
     }
 
@@ -47,36 +52,37 @@ contract Transactions {
         return msg.sender == owner;
     }
 
-    function donateForFood(uint amount, string memory message, string memory keyword) public {
+    function donateForFood(string memory message, string memory keyword) public payable {
         transactionCount += 1;
-        totalAmount += amount;
-        transactions.push(TransferStruct(msg.sender, owner, amount, message, block.timestamp, keyword, "-1"));
+        totalAmount += msg.value;
+        transactions.push(TransferStruct(msg.sender, owner, msg.value, message, block.timestamp, keyword, "-1"));
+        owner.transfer(msg.value);
 
-        emit Transfer(msg.sender, owner, amount, message, block.timestamp, keyword, "-1");
+        emit Transfer(msg.sender, owner, msg.value, message, block.timestamp, keyword, "-1");
     }
 
-    function donateForPuppy(string memory dogId, uint amount, string memory message, string memory keyword) public {
+    function donateForPuppy(string memory dogId, string memory message, string memory keyword) public payable {
         transactionCount += 1;
-        totalAmount += amount;
-        transactions.push(TransferStruct(msg.sender, owner, amount, message, block.timestamp, keyword, dogId));
-        puppyMap[dogId].push(TransferStruct(msg.sender, owner, amount, message, block.timestamp, keyword, dogId));
+        totalAmount += msg.value;
+        transactions.push(TransferStruct(msg.sender, owner, msg.value, message, block.timestamp, keyword, dogId));
+        owner.transfer(msg.value);
 
-        emit Transfer(msg.sender, owner, amount, message, block.timestamp, keyword, dogId);
+        emit Transfer(msg.sender, owner, msg.value, message, block.timestamp, keyword, dogId);
     }
 
-    function addPuppy(string memory dogId, string memory imageUrl) public onlyOwner {
-        puppies.push(Puppy(dogId, imageUrl));
+    function addPuppy(string memory imageUrl, string memory name, string memory birthday, string memory description) public onlyOwner {
+        bytes32 dogId = keccak256(abi.encodePacked(block.timestamp));
+        while(puppyIdSet[dogId]){
+            dogId = keccak256(abi.encodePacked(block.timestamp));
+        }
+        puppyIdSet[dogId] = true;
+        puppies.push(Puppy(dogId, imageUrl, name, birthday, description));
 
-        emit PuppyAdded(dogId, imageUrl, block.timestamp);
+        emit PuppyAdded(dogId, imageUrl, name, birthday, description, block.timestamp);
     }
 
     function getAllPuppies() public view returns (Puppy[] memory) {
         return puppies;
-    }
-
-    // get donate details by dogId
-    function getPuppyDonateDetailById(string memory dogId) public view returns (TransferStruct[] memory) {
-        return puppyMap[dogId];
     }
 
     // all transactions (optimize: paging)
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './transactions.json'
 
 export const contractABI = abi.abi
-export const contractAddress = '0xBEF09F31a69A6aC106BD4F788e423FE53FB11f91'
+export const contractAddress = '0x5bF0c98D9d78878f9cAB04C308EF100B2103438A'
