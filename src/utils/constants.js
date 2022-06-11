54eeb2f71e7dd035212076ab4e132901109f9302
Xanonymous <trusaidlin@gmail.com>
2022-06-12 03:30:40 +0800

    style: format files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const contractABI = import ('../assets/constants/puppySponsorContractAbi.json')
+export const contractABI = import('../assets/constants/puppySponsorContractAbi.json')
 export const contractAddress = '0x079B9F07c4F23ee17F72C33CA9D618F79B000eFb'
