ae58dc8730e884b59a53d18d9d128398887b3b58
Xanonymous <trusaidlin@gmail.com>
2022-06-13 02:40:44 +0800

    refactor: migrate transaction context to typescript

--- src/context/TransactionContext.tsx
+++ src/context/TransactionContext.tsx
@@ -1,39 +1,46 @@
 import React, { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { Contract, ethers, providers } from 'ethers'
 
 import { contractABI, contractAddress } from '../utils/constants'
+import { useSetState } from 'react-use'
 
 export const transactionContext = React.createContext(undefined, undefined)
 
-const { ethereum } = window
+declare global {
+  interface Window {
+    ethereum?: providers.ExternalProvider
+  }
+}
 
-const createEthereumContract = () => {
-  const provider = new ethers.providers.Web3Provider(ethereum)
+const globalWindow: Window = window
+const ethereumProvider = globalWindow.ethereum
+
+const createEthereumContractClient = (): Contract => {
+  const provider = new ethers.providers.Web3Provider(ethereumProvider)
   const signer = provider.getSigner()
   return new ethers.Contract(contractAddress, contractABI, signer)
 }
 
 export const TransactionsProvider = ({ children }) => {
-  const [formData, setFormData] = useState({ amount: '', keyword: '', message: '' })
+  const [formData, setFormData] = useSetState({ amount: '', keyword: '', message: '' })
   const [currentAccount, setCurrentAccount] = useState('')
   const [isLoading, setIsLoading] = useState(false)
-  const [donateTransactionAmount, setDonateTransactionAmount] = useState(
-    localStorage.getItem('donateTransactionAmount')
-  )
   const [transactions, setTransactions] = useState([])
   const [puppies, setPuppies] = useState([])
 
-  const contract = createEthereumContract()
+  const [txState, setTxState] = useSetState({})
 
-  const handleChange = (e, name) => {
-    setFormData((prevState) => ({ ...prevState, [name]: e.target.value }))
+  const contract = createEthereumContractClient()
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>, name: string): void => {
+    setFormData((prevState: typeof formData) => ({ ...prevState, [name]: e.target.value }))
   }
 
   const getAllTransactions = async () => {
     try {
-      if (ethereum) {
+      if (ethereumProvider) {
         const availableTransactions = await contract.getAllDonateTransactions()
-        console.log('availableTransactions', availableTransactions)
+        console.info('availableTransactions', availableTransactions)
 
         const structuredTransactions = availableTransactions.map((transaction) => ({
           addressFrom: transaction.donor,
@@ -47,137 +54,113 @@ export const TransactionsProvider = ({ children }) => {
 
         setTransactions(structuredTransactions)
       } else {
-        console.log('Ethereum is not present')
+        console.info('Ethereum is not present')
       }
     } catch (error) {
-      console.log(error)
+      console.warn(error)
     }
   }
 
   const getAllPuppies = async () => {
     try {
-      if (ethereum) {
+      if (ethereumProvider) {
         const availablePuppies = await contract.getAllPuppies()
 
-        console.log('puppies', availablePuppies)
+        console.info('puppies', availablePuppies)
 
         setPuppies(availablePuppies)
       } else {
-        console.log('Ethereum is not present')
+        console.info('Ethereum is not present')
       }
     } catch (error) {
-      console.log(error)
+      console.warn(error)
     }
   }
 
   const checkIfWalletIsConnect = async () => {
     try {
-      if (!ethereum) return alert('Please install MetaMask.')
+      if (!ethereumProvider) return alert('Please install MetaMask.')
 
-      const accounts = await ethereum.request({ method: 'eth_accounts' })
+      const accounts = await ethereumProvider.request({ method: 'eth_accounts' })
 
       if (accounts.length) {
         setCurrentAccount(accounts[0])
         await getAllPuppies()
         await getAllTransactions()
       } else {
-        console.log('No accounts found')
+        console.info('No accounts found')
       }
     } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const checkIfTransactionsExists = async () => {
-    try {
-      if (ethereum) {
-        const currentDonateTransactionAmount = await contract.donateTransactionAmount()
-
-        window.localStorage.setItem('donateTransactionAmount', currentDonateTransactionAmount)
-      }
-    } catch (error) {
-      console.log(error)
-      throw new Error('No ethereum object')
+      console.warn(error)
     }
   }
 
   const connectWallet = async () => {
     try {
-      if (!ethereum) return alert('Please install MetaMask.')
+      if (!ethereumProvider) return alert('Please install MetaMask.')
 
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+      const accounts = await ethereumProvider.request({ method: 'eth_requestAccounts' })
 
       setCurrentAccount(accounts[0])
-      window.location.reload()
+      setTxState(() => {})
     } catch (error) {
-      console.log(error)
-      throw new Error('No ethereum object')
+      console.warn('No ethereum object', error)
     }
   }
 
   const donateForFood = async () => {
     try {
-      if (ethereum) {
+      if (ethereumProvider) {
         const { amount, keyword, message } = formData
 
         const options = { value: ethers.utils.parseEther(amount) }
         const transactionHash = await contract.donateForFood(message, keyword, options)
 
         setIsLoading(true)
-        console.log(`Loading - ${transactionHash.hash}`)
+        console.info(`Loading - ${transactionHash.hash}`)
         await transactionHash.wait()
-        console.log(`Success - ${transactionHash.hash}`)
+        console.info(`Success - ${transactionHash.hash}`)
         setIsLoading(false)
 
-        const transactionsCount = await contract.donateTransactionAmount()
-
-        setDonateTransactionAmount(transactionsCount.toNumber())
-        window.location.reload()
+        setTxState(() => {})
       } else {
-        console.log('No ethereum object')
+        console.info('No ethereum object')
       }
     } catch (error) {
-      console.log(error)
-      throw new Error('No ethereum object')
+      console.warn('No ethereum object', error)
     }
   }
 
   const donateForPuppy = async (puppyId) => {
     try {
-      if (ethereum) {
+      if (ethereumProvider) {
         const { amount, keyword, message } = formData
 
         const options = { value: ethers.utils.parseEther(amount) }
         const transactionHash = await contract.donateForPuppy(puppyId, message, keyword, options)
 
         setIsLoading(true)
-        console.log(`Loading - ${transactionHash.hash}`)
+        console.info(`Loading - ${transactionHash.hash}`)
         await transactionHash.wait()
-        console.log(`Success - ${transactionHash.hash}`)
+        console.info(`Success - ${transactionHash.hash}`)
         setIsLoading(false)
 
-        const transactionsCount = await contract.donateTransactionAmount()
-
-        setDonateTransactionAmount(transactionsCount.toNumber())
-        window.location.reload()
+        setTxState(() => {})
       } else {
-        console.log('No ethereum object')
+        console.info('No ethereum object')
       }
     } catch (error) {
-      console.log(error)
-      throw new Error('No ethereum object')
+      console.warn('No ethereum object', error)
     }
   }
 
   useEffect(() => {
-    checkIfWalletIsConnect()
-    checkIfTransactionsExists()
-  }, [donateTransactionAmount])
+    checkIfWalletIsConnect().then()
+  }, [txState])
 
   return (
     <transactionContext.Provider
       value={{
-        donateTransactionAmount,
         connectWallet,
         transactions,
         puppies,
