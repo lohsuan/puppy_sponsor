5e0e7ef559f5ad37c8fefff52d30f1d70752aca8
lohsuan <angelalo88362@gmail.com>
2022-06-22 17:02:41 +0800

    feat: add processing effect when transfering.

--- src/components/PuppyToken.jsx
+++ src/components/PuppyToken.jsx
@@ -69,13 +69,23 @@ const PuppyTokenPage = () => {
           required
           className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
         />
-        <button
-          type="button"
-          onClick={handleSubmit}
-          className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
-        >
-          Send now
-        </button>
+        {isProcessing ?
+          <button type="button" disabled className="flex items-center justify-center text-white w-full mt-2 border-[1px] p-2 border-[#8e9fc9d5] rounded-full cursor-not-allowed">
+            <svg class="animate-spin w-5 mr-2 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            Processing...
+          </button>
+          :
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+          >
+            Send now
+          </button>
+        }
       </div>
     </div>
   )
