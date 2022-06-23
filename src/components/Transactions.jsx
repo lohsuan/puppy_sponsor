d1ad5e38449766cc4bc95bda927ae4270660794b
tzulin <tzulin0517@gmail.com>
2022-06-23 17:37:30 +0800

    style: format files

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -54,7 +54,9 @@ const Transactions = (props) => {
     <div className="flex min-h-screen w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
       <div className="flex flex-col md:p-12 py-12 px-4">
         {currentAccount ? (
-          <h3 className="text-white text-3xl text-center my-2">{props.content === 'all' ? "Latest Transactions" : "My Transactions"}</h3>
+          <h3 className="text-white text-3xl text-center my-2">
+            {props.content === 'all' ? 'Latest Transactions' : 'My Transactions'}
+          </h3>
         ) : (
           <h3 className="text-white text-3xl text-center my-2">
             Connect your account to see the transactions
