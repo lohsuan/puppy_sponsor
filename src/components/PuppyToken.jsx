18f45d2304201fca30abe38754f5cce849d4c490
tzulin <tzulin0517@gmail.com>
2022-06-23 16:45:03 +0800

    feat: change PUPPY token icon

--- src/components/PuppyToken.jsx
+++ src/components/PuppyToken.jsx
@@ -138,22 +138,7 @@ const PuppyTokenPage = () => {
             </p>
             <div className="m-auto p-5 max-w-[90vw] xl:max-w-[70vw] md:w-auto rounded-lg border shadow-md border-gray-700 bg-gray-800">
               <div className="flex items-center space-x-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  stroke="currentColor"
-                  className="w-6 h-6 text-white"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="m12 14 9-5-9-5-9 5 9 5z" />
-                  <path d="m12 14 6.16-3.422a12.083 12.083 0 0 1 .665 6.479A11.952 11.952 0 0 0 12 20.055a11.952 11.952 0 0 0-6.824-2.998 12.078 12.078 0 0 1 .665-6.479L12 14z" />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m12 14 9-5-9-5-9 5 9 5zm0 0 6.16-3.422a12.083 12.083 0 0 1 .665 6.479A11.952 11.952 0 0 0 12 20.055a11.952 11.952 0 0 0-6.824-2.998 12.078 12.078 0 0 1 .665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  />
-                </svg>
+                <img src="/puppy_token_icon.png" alt="token_icon" className="h-16 cursor-pointer" />
                 <div className="space-y-1 font-medium text-white">
                   <div>{tokenSymbol}</div>
                   <div className="text-sm text-gray-400">{tokenAmounts}</div>
