cb75f86d4554ea47528a1cfd310fc3878824a820
lohsuan <angelalo88362@gmail.com>
2022-06-24 14:15:12 +0800

    hotfix: change birthday input type from text to date.

--- src/components/AddPuppy.jsx
+++ src/components/AddPuppy.jsx
@@ -240,7 +240,7 @@ const AddPuppyPage = () => {
                 </div>
                 <input
                   id="new-puppy-date-picker"
-                  type="text"
+                  type="date"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Please enter with format yyyy/mm/dd"
                   onChange={(el) => {
