a5b0c31359492da9a7dfad30531fd1d831e3e9df
Xanonymous <trusaidlin@gmail.com>
2022-08-13 17:41:56 +0800

    chore: upgrade to vite3

--- vite.config.ts
+++ vite.config.ts
@@ -6,22 +6,7 @@ import { createHtmlPlugin } from 'vite-plugin-html'
 export default defineConfig({
   plugins: [react(), createHtmlPlugin()],
   build: {
-    minify: 'terser',
-    brotliSize: true,
     cssCodeSplit: true,
-    terserOptions: {
-      sourceMap: false,
-      ecma: 5,
-      ie8: true,
-      safari10: true,
-      keep_classnames: false,
-      keep_fnames: false,
-      compress: true,
-      format: {
-        preserve_annotations: false,
-        comments: false
-      }
-    },
     chunkSizeWarningLimit: 100000,
     rollupOptions: {
       output: {
